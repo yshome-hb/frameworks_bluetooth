000d78ac065470dc01ac728731363b3b83b3b2b6
zhongzhijie1 <zhongzhijie1@xiaomi.com>
2024-10-24 14:51:11 +0800

    [feat] Implement easy insertion of timeval measurements.
    bug: V/43595
    
    Changes:
    1. Enabled Kconfig option BLUETOOTH_DEBUG_TIMEVAL, choice BLUETOOTH_DEBUG_TIME_UNIT_US enable or not.
    2. Included 'bt_debug.h' in Bluetooth service or Bluetooth stack (bluelet).
    3. Usage instructions:
       - In any function, first define the timeval structure by calling BT_DEBUG_MKTIMEVAL_S(name_s); at the global scope of the .c file.
       - Then, within the function, call BT_DEBUG_ENTER_TIME_SECTION(name_s); to record the entry time.
       - Finally, call BT_DEBUG_EXIT_TIME_SECTION(name_s); to record the exit time.
    
    Example:
    
    BT_DEBUG_MKTIMEVAL_S(hci_poll_recv);  // Suggest using the function name; since '__func__' cannot be used in precompile state, you need to input it manually.
    
    static void hci_poll_recv(service_poll_t* poll, int revent, void* userdata)
    {
        (void)poll;
        (void)userdata;
    
        if (revent & (POLL_ERROR | POLL_DISCONNECT))
            hci_remove_recv(NULL);
    
        if (revent & POLL_READABLE) {
            BT_DEBUG_ENTER_TIME_SECTION(hci_poll_recv); // Record entry time.
            bt_sal_hci_transport_recv();
            BT_DEBUG_EXIT_TIME_SECTION(hci_poll_recv);  // Record exit time.
        }
    }
    
    Signed-off-by: zhongzhijie1 <zhongzhijie1@xiaomi.com>

--- framework/include/bt_debug.h
+++ framework/include/bt_debug.h
@@ -5,6 +5,69 @@
 extern "C" {
 #endif
 
+#include "bt_time.h"
+
+/* Time value structure for Bluetooth debug timing */
+typedef struct timeval_s {
+    uint64_t entry_time;
+    uint64_t exit_time;
+    uint64_t max_timeval;
+    uint64_t last_timeval;
+} bt_timeval_t;
+
+/* Define macro to create a timeval structure for debugging */
+#ifdef CONFIG_BLUETOOTH_DEBUG_TIMEVAL
+#define BT_DEBUG_MKTIMEVAL_S(name_s) \
+    bt_timeval_t g_timeval_##name_s = { 0, 0, 0, 0 }
+#else
+#define BT_DEBUG_MKTIMEVAL_S(name_s) /* No operation */
+#endif
+
+/* Macro to get current timestamp */
+#ifdef CONFIG_BLUETOOTH_DEBUG_TIMEVAL
+#ifdef CONFIG_BLUETOOTH_DEBUG_TIME_UNIT_US
+#define _GetCurrTime() ((uint64_t)get_os_timestamp_us())
+#else
+#define _GetCurrTime() ((uint64_t)get_os_timestamp_ms())
+#endif
+#else
+#define _GetCurrTime() 0
+#endif
+
+/* Macro to mark entry of a time section */
+#ifdef CONFIG_BLUETOOTH_DEBUG_TIMEVAL
+#define BT_DEBUG_ENTER_TIME_SECTION(name_s)             \
+    do {                                                \
+        g_timeval_##name_s.entry_time = _GetCurrTime(); \
+    } while (0)
+#else
+#define BT_DEBUG_ENTER_TIME_SECTION(name_s) /* No operation */
+#endif
+
+/* Macro to mark exit of a time section */
+#ifdef CONFIG_BLUETOOTH_DEBUG_TIMEVAL
+#define BT_DEBUG_EXIT_TIME_SECTION(name_s)                                                                      \
+    do {                                                                                                        \
+        if (g_timeval_##name_s.entry_time != 0) {                                                               \
+            g_timeval_##name_s.exit_time = _GetCurrTime();                                                      \
+            if (g_timeval_##name_s.exit_time >= g_timeval_##name_s.entry_time) {                                \
+                g_timeval_##name_s.last_timeval = g_timeval_##name_s.exit_time - g_timeval_##name_s.entry_time; \
+                if (g_timeval_##name_s.last_timeval > g_timeval_##name_s.max_timeval) {                         \
+                    g_timeval_##name_s.max_timeval = g_timeval_##name_s.last_timeval;                           \
+                }                                                                                               \
+            } else {                                                                                            \
+                /* if exit_time < entry_time, reset all to zero */                                              \
+                g_timeval_##name_s.entry_time = 0;                                                              \
+                g_timeval_##name_s.exit_time = 0;                                                               \
+                g_timeval_##name_s.last_timeval = 0;                                                            \
+                g_timeval_##name_s.max_timeval = 0;                                                             \
+            }                                                                                                   \
+        }                                                                                                       \
+    } while (0)
+#else
+#define BT_DEBUG_EXIT_TIME_SECTION(name_s) /* No operation */
+#endif
+
 #if defined(__NuttX__) // Vela
 
 #include <debug.h>
