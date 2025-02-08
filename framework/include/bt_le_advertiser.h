510764ce627658026e4014e8fb6452f7719ac119
chejinxian1 <chejinxian1@xiaomi.com>
2025-02-08 18:04:23 +0800

    API Comments: Add comments to the API related to PAN and LE Advertiser.
    bug: v/49546
    
    Rootcause:To better assist the use of Bluetooth services for Bluetooth applications, this submission will add commentary descriptions for certain APIs in order to provide instructions for application reference.
    
    Signed-off-by: chejinxian1 <chejinxian1@xiaomi.com>

--- framework/include/bt_le_advertiser.h
+++ framework/include/bt_le_advertiser.h
@@ -26,6 +26,10 @@ extern "C" {
 #endif
 
 /**
+ * @cond
+ */
+
+/**
  * @brief Advertising type
  *
  */
@@ -88,22 +92,56 @@ enum {
 typedef void bt_advertiser_t;
 
 /**
- * @brief Advertising start status callback, invoke on adverting start success or failure
+ * @endcond
+ */
+
+/**
+ * @brief Callback for advertising started notification.
  *
- * @param adv - advertiser handle.
- * @param adv_id - advertiser ID.
- * @param status - advertiser start status, BT_ADV_STATUS_SUCCESS on success.
+ * This callback is used to notify the application of the adverter handle, ID, and
+ * start status of the advertiser. It will be triggered in the following cases:
+ * 1. Advertiser ID allocates failed or the return value of the function "bt_sal_le_start_adv"
+ *    is not "BT_STATUS_SUCCESS" within the advertiser starting event.
+ * 2. 1 second after the successful notification of the start of LE advertising to the
+ *    Bluetooth protocol stack.
  *
+ * @param adv - Notifies the allocated Advertiser handle.
+ * @param adv_id - Notifies the allocated Advertiser ID.
+ * @param status - Notifies the starting status of the advertiser. BT_ADV_STATUS_SUCCESS
+ *                 indicates that the advertiser has started successfully.
+ *
+ * **Example:**
+ * @code
+void on_advertising_start(bt_advertiser_t* adv, uint8_t adv_id, uint8_t status)
+{
+    printf("on_advertising_start, adv_id: %d, status: %d\n", adv_id, status);
+}
+ * @endcode
  */
 typedef void (*on_advertising_start_cb_t)(bt_advertiser_t* adv, uint8_t adv_id, uint8_t status);
 
 /**
- * @brief Advertising stopped notification
+ * @brief Callback for advertising stopped notification.
  *
- * @param adv - advertiser handle
- * @param adv_id - advertiser ID.
+ * This callback is used to notify the application of the handle and ID corresponding to
+ * the stopped advertising.
+ *
+ * @param adv - Advertiser handle.
+ * @param adv_id - Advertiser ID.
+ *
+ * **Example:**
+ * @code
+void on_advertising_stopped(bt_advertiser_t* adv, uint8_t adv_id)
+{
+    printf("on_advertising_stopped, adv_id: %d\n", adv_id);
+}
+ * @endcode
  */
 typedef void (*on_advertising_stopped_cb_t)(bt_advertiser_t* adv, uint8_t adv_id);
+
+/**
+ * @cond
+ */
 
 /**
  * @brief Advertising callback functions structure
@@ -130,16 +168,58 @@ typedef struct {
 } ble_adv_params_t;
 
 /**
- * @brief Start LE advertising
+ * @endcond
+ */
+
+/**
+ * @brief Initate LE advertising.
  *
- * @param ins - bluetooth client instance.
- * @param params - advertising parameter.
- * @param adv_data - advertisement data.
- * @param adv_len - length of advertisement data.
- * @param scan_rsp_data - scan response data.
- * @param scan_rsp_len - length of scan response data.
- * @param cbs - advertiser callback functions.
- * @return bt_advertiser_t* - advertiser handle.
+ * Before using this function to initiate LE advertising, the Bluetooth application
+ * should set appropriate LE advertising parameters, prepare advertising data and scan
+ * response data, and an advertising callback function. When using this function, the
+ * above content is passed as parameters, and an advertiser handle is returned to indicate
+ * the initiated advertising. With this handle, the Bluetooth application can disable the
+ * corresponding advertising at an appropriate time.
+ *
+ * @param ins - Bluetooth client instance.
+ * @param params - Advertising parameter.
+ * @param adv_data - Advertisement data.
+ * @param adv_len - Length of advertisement data.
+ * @param scan_rsp_data - Scan response data.
+ * @param scan_rsp_len - Length of scan response data.
+ * @param cbs - Advertiser callback functions.
+ * @return bt_advertiser_t* - Advertiser handle.
+ *
+ * **Example:**
+ * @code
+bt_advertiser_t* adver;
+
+void app_start_advertising(bt_instance_t* ins)
+{
+    ble_adv_params_t params;
+    uint8_t adv_data[10];
+    uint8_t scan_rsp_data[10];
+    advertiser_callback_t cbs;
+
+
+    params.adv_type = BT_LE_ADV_IND;
+    params.own_addr_type = BT_LE_ADDR_TYPE_PUBLIC;
+    params.tx_power = -20;
+    params.interval = 100;
+    params.duration = 0;
+    params.channel_map = 7;
+    params.filter_policy = BT_LE_ADV_FILTER_WHITE_LIST_FOR_ALL;
+
+    memset(adv_data, 0, sizeof(adv_data));
+    memset(scan_rsp_data, 0, sizeof(scan_rsp_data));
+
+    cbs.size = sizeof(advertiser_callback_t);
+    cbs.on_advertising_start = on_advertising_start;
+    cbs.on_advertising_stopped = on_advertising_stopped;
+
+    adver = bt_le_start_advertising(ins, &params, adv_data, sizeof(adv_data), scan_rsp_data, sizeof(scan_rsp_data), &cbs);
+}
+ * @endcode
  */
 bt_advertiser_t* BTSYMBOLS(bt_le_start_advertising)(bt_instance_t* ins,
     ble_adv_params_t* params,
@@ -150,27 +230,69 @@ bt_advertiser_t* BTSYMBOLS(bt_le_start_advertising)(bt_instance_t* ins,
     advertiser_callback_t* cbs);
 
 /**
- * @brief Stop LE advertising by advertiser handle
+ * @brief Stop LE advertising by advertiser handle.
  *
- * @param ins - bluetooth client instance.
- * @param adver - advertiser handle.
+ * This function is used to stop the LE advertising indicated by the specified advertiser
+ * handle. Therefore, before using this function, the Bluetooth application should know
+ * the handle corresponding to the initiated advertising. When a Bluetooth application
+ * initiates an advertising, it will be informed of the advertiser handle.
+ *
+ * @param ins - Bluetooth client instance.
+ * @param adver - Advertiser handle.
+ *
+ * **Example:**
+ * @code
+void app_stop_advertising(bt_instance_t* ins)
+{
+    if(!adver)
+        return;
+    bt_le_stop_advertising(ins, adver);
+}
+ * @endcode
  */
 void BTSYMBOLS(bt_le_stop_advertising)(bt_instance_t* ins, bt_advertiser_t* adver);
 
 /**
- * @brief Stop LE advertising by adver id
+ * @brief Stop LE advertising by advertiser ID.
  *
- * @param ins - bluetooth client instance.
- * @param adv_id - advertiser ID.
+ * This function is used to stop the LE advertising indicated by the specified advertiser ID.
+ * Therefore, before using this function, the Bluetooth application should know the ID
+ * corresponding to the initiated advertising. When a Bluetooth application successfully initiates
+ * an advertising, the advertiser ID will be informed through the "on_advertising_start_cb_t"
+ * callback function provided when the advertising is initiated by the application.
+ *
+ * @param ins - Bluetooth client instance.
+ * @param adv_id - Advertiser ID.
+ *
+ * **Example:**
+ * @code
+void app_stop_advertising(bt_instance_t* ins, uint8_t adv_id)
+{
+    bt_le_stop_advertising_id(ins, adv_id);
+}
+ * @endcode
  */
 void BTSYMBOLS(bt_le_stop_advertising_id)(bt_instance_t* ins, uint8_t adv_id);
 
 /**
- * @brief Check is advertising supported
+ * @brief Check if LE advertising is supported.
  *
- * @param ins - bluetooth client instance.
- * @return true - support.
- * @return false - not support.
+ * This function is used to check if the Bluetooth protocol stack supports LE advertising for
+ * Bluetooth applications.
+ *
+ * @param ins - Bluetooth client instance.
+ * @return bool - true represents support for LE advertising, while false represents non-support.
+ *
+ * **Example:**
+ * @code
+void app_check_advertising_support(bt_instance_t* ins)
+{
+    if(bt_le_advertising_is_supported(ins))
+        printf("advertising is supported\n");
+    else
+        printf("advertising is not supported\n");
+}
+ * @endcode
  */
 bool BTSYMBOLS(bt_le_advertising_is_supported)(bt_instance_t* ins);
 
--- framework/include/bt_pan.h
+++ framework/include/bt_pan.h
@@ -26,7 +26,11 @@
 #endif
 
 /**
- * @brief Pan role type define.
+ * @cond
+ */
+
+/**
+ * @brief PAN role type define.
  *
  */
 typedef enum {
@@ -50,28 +54,67 @@ typedef enum {
 } pan_netif_state_t;
 
 /**
- * @brief Pan connection state change callback.
+ * @endcond
+ */
+
+/**
+ * @brief Callback for PAN connection state change.
  *
- * @param cookie - callbacks cookie, the return value of bt_pan_register_callbacks.
- * @param state - pan connection state
- * @param bd_addr - address of peer device.
- * @param local_role - local device pan role, reference type pan_role_t.
- * @param remote_role - remote device pan role, reference type pan_role_t.
+ * When the PAN connection state changes, this callback will be triggered to
+ * notify the cookie corresponding to the callback, the latest PAN connection
+ * state, the Bluetooth address of the peer device, and the roles of both
+ * devices in the PAN.
+ *
+ * @param cookie - Callbacks cookie, the return value of bt_pan_register_callbacks.
+ * @param state - PAN connection state
+ * @param bd_addr - The Bluetooth address of the peer device.
+ * @param local_role - Local device PAN role, reference type pan_role_t.
+ * @param remote_role - Remote device PAN role, reference type pan_role_t.
+ *
+ * **Example:**
+ * @code
+void pan_connection_state_cb(void* cookie, profile_connection_state_t state,
+    bt_address_t* bd_addr, uint8_t local_role,
+    uint8_t remote_role)
+{
+    printf("pan_connection_state_cb, state: %d, bd_addr: %s, local_role: %d, remote_role: %d\n",
+        state, bd_addr->address, local_role, remote_role);
+}
+ * @endcode
  */
 typedef void (*pan_connection_state_callback)(void* cookie, profile_connection_state_t state,
     bt_address_t* bd_addr, uint8_t local_role,
     uint8_t remote_role);
 
 /**
- * @brief Pan net interface (down/up)state change callback.
+ * @brief Callback for PAN net interface (down/up) state change.
  *
- * @param cookie  - callbacks cookie, the return value of bt_pan_register_callbacks.
- * @param state - net interface state.
- * @param local_role - local device pan role
- * @param ifname - net interface name, default "bt-pan".
+ * When the PAN interface becomes up or down, this callback will be triggered
+ * to notify the Cookie corresponding to the callback, the latest status of
+ * the interface, the role of the local device in the PAN, and the interface
+ * name.
+ *
+ * @param cookie  - Callbacks cookie, the return value of bt_pan_register_callbacks.
+ * @param state - Net interface state.
+ * @param local_role - Local device PAN role.
+ * @param ifname - Net interface name, default "bt-pan".
+ *
+ * **Example:**
+ * @code
+void pan_netif_state_cb(void* cookie, pan_netif_state_t state,
+    int local_role, const char* ifname)
+{
+    printf("pan_netif_state_cb, state: %d, local_role: %d, ifname: %s\n",
+        state, local_role, ifname);
+}
+ * @endcode
  */
 typedef void (*pan_netif_state_callback)(void* cookie, pan_netif_state_t state,
     int local_role, const char* ifname);
+
+/**
+ * @cond
+ */
 
 /**
  * @brief PAN event callbacks structure
@@ -84,41 +127,133 @@ typedef struct {
 } pan_callbacks_t;
 
 /**
- * @brief Register callback functions to pan service
+ * @endcond
+ */
+
+/**
+ * @brief Register callback functions to PAN service.
  *
- * @param ins - bluetooth client instance.
- * @param callbacks - pan callback functions.
- * @return void* - callback cookie, NULL on failure.
+ * This function is used to register the callback for notifying PAN connection
+ * states and the callback for notifying PAN interface states to the PAN service.
+ * When the PAN service detects the corresponding event, it will notify the
+ * application through the registered callback function. After this function is
+ * called, it returns the cookie corresponding to the callback.
+ *
+ * @param ins - Bluetooth client instance.
+ * @param callbacks - PAN callback functions.
+ * @return void* - Callback cookie, NULL represents fail.
+ *
+ * **Example:**
+ * @code
+void* pan_cookie;
+const static pan_callbacks_t callbacks = {
+    .size = sizeof(pan_callbacks_t),
+    .netif_state_cb = pan_netif_state_cb,
+    .connection_state_cb = pan_connection_state_cb,
+};
+
+void app_init_pan(bt_instance_t* ins)
+{
+    pan_cookie = bt_pan_register_callbacks(ins, &callbacks);
+    if (!pan_cookie)
+        printf("register pan callbacks failed\n");
+    else
+        printf("register pan callbacks success\n");
+}
+ * @endcode
  */
 void* BTSYMBOLS(bt_pan_register_callbacks)(bt_instance_t* ins, const pan_callbacks_t* callbacks);
 
 /**
- * @brief Unregister pan callback function
+ * @brief Unregister PAN callback functions.
  *
- * @param ins - bluetooth client instance.
- * @param cookie - callbacks cookie.
+ * This function is used to unregister callbacks from the PAN service. The caller
+ * needs to provide the cookie corresponding to callbacks.
+ *
+ * @param ins - Bluetooth client instance.
+ * @param cookie - Callbacks cookie.
  * @return true - on callback unregister success
  * @return false - on callback cookie not found
+ *
+ * **Example:**
+ * @code
+void app_deinit_pan(bt_instance_t* ins)
+{
+    bt_status_t status;
+    if (!pan_cookie)
+        return;
+
+    status = bt_pan_unregister_callbacks(ins, pan_cookie);
+    if (status != BT_STATUS_SUCCESS)
+        printf("unregister pan callbacks failed\n");
+    else
+        printf("unregister pan callbacks success\n");
+}
+ * @endcode
  */
 bool BTSYMBOLS(bt_pan_unregister_callbacks)(bt_instance_t* ins, void* cookie);
 
 /**
- * @brief  Connect to pan device
+ * @brief  Connect to PAN device.
  *
- * @param ins - bluetooth client instance.
- * @param addr - address of peer device.
- * @param dst_role - dest pan role, reference type pan_role_t.
- * @param src_role - src pan role, reference type pan_role_t.
+ * This function is used to connect to a PAN device. The caller needs to provide
+ * the address of the peer device, the role of the local device, and the role of
+ * the peer device.
+ *
+ * @param ins - Bluetooth client instance.
+ * @param addr - The Bluetooth address of the peer device.
+ * @param dst_role - Destination PAN role, reference type pan_role_t.
+ *                   Only support PANU connect to NAP server, dst_role must be PAN_ROLE_NAP.
+ * @param src_role - Source PAN role, reference type pan_role_t.
+ *                   Only support PANU connect to NAP server, src_role must be PAN_ROLE_PANU.
  * @return bt_status_t - BT_STATUS_SUCCESS on success, a negated errno value on failure.
+ *
+ * **Example:**
+ * @code
+void app_connect_pan_device(bt_instance_t* ins)
+{
+    bt_status_t status;
+    bt_address_t addr = {
+        .addr = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06},
+    };
+
+    status = bt_pan_connect(ins, &addr, PAN_ROLE_NAP, PAN_ROLE_PANU);
+    if(status != BT_STATUS_SUCCESS)
+        printf("connect pan device failed\n");
+    else
+        printf("connect pan device success\n");
+}
+ * @endcode
+
  */
 bt_status_t BTSYMBOLS(bt_pan_connect)(bt_instance_t* ins, bt_address_t* addr, uint8_t dst_role, uint8_t src_role);
 
 /**
- * @brief Disconnect from pan connection
+ * @brief Disconnect from PAN connection.
  *
- * @param ins - bluetooth client instance.
- * @param addr - address of peer device.
+ * This function is used to disconnect from a PAN device. The caller needs to
+ * provide the address of the peer device.
+ *
+ * @param ins - Bluetooth client instance.
+ * @param addr - The Bluetooth address of the peer device.
  * @return bt_status_t - BT_STATUS_SUCCESS on success, a negated errno value on failure.
+ *
+ * **Example:**
+ * @code
+void app_disconnect_pan_device(bt_instance_t* ins)
+{
+    bt_status_t status;
+    bt_address_t addr = {
+        .addr = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06},
+    };
+
+    status = bt_pan_disconnect(ins, &addr);
+    if(status != BT_STATUS_SUCCESS)
+        printf("disconnect pan device failed\n");
+    else
+        printf("disconnect pan device success\n");
+}
+ * @endcode
  */
 bt_status_t BTSYMBOLS(bt_pan_disconnect)(bt_instance_t* ins, bt_address_t* addr);
 
